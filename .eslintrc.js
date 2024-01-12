b66be534bd3a835505a9ae2920658f56c4538644
aroundthistime <62099715+aroundthistime@users.noreply.github.com>
2024-01-12 17:34:01 +0900

    setting: Setup eslint environment (with autocorrect on save)

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "standard-with-typescript",
+        "plugin:react/recommended"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+      'react/function-component-definition': 0,
+      '@typescript-eslint/explicit-function-return-type': 0,
+      '@typescript-eslint/no-non-null-assertion': 0
+    }
+}
